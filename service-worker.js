74e6f7d5aea5b003979275686ac169b66eaaeb1f
smartkinmu <guribow+sumakin@gmail.com>
2024-06-09 17:59:11 +0900

    ソースの見直し

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,9 @@
+// キャッシュバージョンを変更
+const CACHE_NAME = 'pwa-sample-cache-v2';
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('pwa-sample-cache-v1').then(function(cache) {
+        caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll([
                 '/',
                 '/index.html',
@@ -16,7 +19,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-    var cacheWhitelist = ['pwa-sample-cache-v1'];
+    var cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
@@ -32,10 +35,15 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        fetch(event.request).then(function(response) {
-            return response;
-        }).catch(function() {
-            return caches.match(event.request);
+        caches.match(event.request).then(function(response) {
+            // キャッシュに一致するリクエストがあれば返し、一致しなければネットワークから取得
+            return response || fetch(event.request).then(function(response) {
+                // 新しいリクエストをキャッシュに保存
+                return caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
