ca671de4a2f9979604686d2e8c0841b7e799eab2
smartkinmu <guribow+sumakin@gmail.com>
2024-06-13 21:20:34 +0900

    メールアドレスが保存できない問題を修正

--- service-worker.js
+++ service-worker.js
@@ -55,4 +55,15 @@ self.addEventListener('message', function(event) {
             clients.forEach(client => client.navigate(client.url));
         });
     }
+    if (event.data.action === 'sync') {
+        event.waitUntil(
+            caches.keys().then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+        );
+    }
 });
